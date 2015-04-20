f42a2fbede57bbecbd857973d8eaaad6a3844e40
willy.nugraha <willy.nugraha@kompas.com>
2015-04-20 11:47:04 +0700

    page done, tp blm simpan hasil

--- page.php
+++ page.php
@@ -1,7 +1,6 @@
 <?php
 	require "system/connect.php";
 	$_SESSION["nomor"]++;
-	print_r($_SESSION);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -17,28 +16,50 @@
     	<div class="ctn">
 			<div>Pilih Jawaban Yang Paling Sesuai Dengan Diri Kamu</div>
             <ul class="poin">
-                <li class="active-page"></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li <?php if($_SESSION["nomor"] == 1) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 2) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 3) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 4) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 5) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 6) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 7) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 8) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 9) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 10) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 11) { ?> class="active-page" <?php } ?>></li>
+                <li <?php if($_SESSION["nomor"] == 12) { ?> class="active-page" <?php } ?>></li>
             </ul>
             <div class="question">
-            	SAYA ADALAH ORANG YANG<br />
+<?php
+	$query_question = mysql_query("SELECT id, question FROM question WHERE id = ".$_SESSION["nomor"]);
+	while ($row_question = mysql_fetch_array($query_question)) {
+		echo $row_question['question'];
+	}
+?>
+				<br />
                     <div class="radio">
-                        <input id="terencana" type="radio" name="yg" value="terencana">
-                        <label for="terencana">Terencana</label><br />
-                        <input id="spontan" type="radio" name="yg" value="spontan">
-                        <label for="spontan">Spontan</label>
+<?php
+	$query_answer = mysql_query("SELECT id, id_question, answer FROM answer WHERE id_question = ".$_SESSION["nomor"]);
+	while ($row_answer = mysql_fetch_array($query_answer)) {
+?>
+                        <input id="<?=$row_answer['id'];?>" type="radio" name="answer" value="<?=$row_answer['id'];?>">
+                        <label for="<?=$row_answer['id'];?>"><?=$row_answer['answer'];?></label><br />
+<?php
+	}
+?>
                     </div> 
             </div>
-            <div class="nxf"><a href="page-akhir.html">NEXT <i class="fa fa-chevron-circle-right"></i></a> </div>
+<?php
+	if ($_SESSION['nomor'] < 12) {
+?>
+            <div class="nxf"><a href="page.php">NEXT <i class="fa fa-chevron-circle-right"></i></a> </div>
+<?php
+	} else {
+?>
+            <div class="nxf"><a href="#">FINISH <i class="fa fa-chevron-circle-right"></i></a> </div>
+<?php
+	}
+?>
         </div>
     </div>
 </body>
